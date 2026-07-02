11e3e8819eec65be4d779a4f4a34e9a0ec9ca896
Gabriielly . <pco@GRUPOGD.CORP>
2026-07-02 11:32:25 -0300

    Versão 3 - PostgreSQL integrado e acesso pela rede configurado

--- app.py
+++ app.py
@@ -111,4 +111,8 @@ app.register_blueprint(usuarios)
 # ==========================
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=False
+    )
