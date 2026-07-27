c3a26d4e2c172e090b3c2e22f488891c3120ee65
gabyszz <suporte@GRUPOGD.CORP>
2026-07-27 10:24:33 -0300

    Nova versão, já no servidor

--- config/config.py
+++ config/config.py
@@ -11,7 +11,7 @@ class Config:
 
     # Banco de dados PostgreSQL
     SQLALCHEMY_DATABASE_URI = (
-    "postgresql+psycopg2://postgres:Mello%4008@localhost:5432/gd_topografia"
+    "postgresql+psycopg2://postgres:Gd%40102030@localhost:5432/gd_topografia"
 )
     # Desabilita aviso do SQLAlchemy
     SQLALCHEMY_TRACK_MODIFICATIONS = False
